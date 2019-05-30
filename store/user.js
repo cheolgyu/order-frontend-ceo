fa2790ad4e79cf7488ef30e0a006a1a7d76a961c
cg <rb7480@naver.com>
2019-05-30 10:41:54 +0900

    로그인 후 내정보 가져오기

--- store/user.js
+++ store/user.js
@@ -1,25 +1,45 @@
 export const state = () => ({
-  auth: null
-})
+  auth: null,
+  user: null
+});
 
 export const actions = {
   async join({ commit }, params) {
-    return await this.$axios.put(`auth`, params).then(res => {
-      return res;
-    }).catch(error => { return error.response });
+    return await this.$axios
+      .put(`auth`, params)
+      .then(res => {
+        return res;
+      })
+      .catch(error => {
+        return error.response;
+      });
   },
-  async login({ commit }, params) {
+  async login({ commit, dispatch }, params) {
     return await this.$axios.post(`auth`, params).then(res => {
       if (res.status === 200) {
-        commit('SET_AUTH', res.data)
+        commit("SET_AUTH", res.data);
+        dispatch("getme");
       }
-      return res.status
-    })
+      return res.status;
+    });
+  },
+  async getme({ commit, rootState }) {
+    return await this.$axios
+      .get(`/users/` + rootState.user.auth.user.id)
+      .then(res => {
+        if (res.status === 200) {
+          commit("SET_USER", res.data);
+        }
+        return res.status;
+      });
   }
-}
+};
 
 export const mutations = {
   SET_AUTH(state, params) {
-    state.auth = params
+    state.auth = params;
+  },
+  SET_USER(state, params) {
+    state.user = params;
   }
-}
+};
