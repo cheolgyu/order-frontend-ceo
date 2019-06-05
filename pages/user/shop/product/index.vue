353fc7ae1664bc766201a9e5d8f139de75968913
성재노트북 <rb7480@knou.ac.kr>
2019-06-05 19:57:14 +0900

    상품 상세

--- pages/user/shop/product/index.vue
+++ pages/user/shop/product/index.vue
@@ -1,131 +1,55 @@
 <template>
   <v-layout row wrap>
-    <!--
     <v-flex xs12 sm5 md5>
       <v-card dark color="primary">
+        <v-btn fab dark small color="indigo">
+          <v-icon dark>get</v-icon>
+        </v-btn>
         <div class="text-xs-center">
           <v-btn fab dark small color="green">
             <v-icon dark>remove</v-icon>
           </v-btn>
-          <v-btn fab dark small color="indigo">
+          <v-btn fab dark small color="indigo" @click="dialog.on = true">
             <v-icon dark>add</v-icon>
           </v-btn>
-          <v-chip color="orange" text-color="white">
-            Premium
-            <v-icon right>star</v-icon>
-          </v-chip>
         </div>
         <v-card-text>
           <v-list>
-            <v-list-tile v-for="item in items" :key="item.title" avatar @click>
-              <v-list-tile-action>
-                <v-icon v-if="item.icon" color="pink">star</v-icon>
-              </v-list-tile-action>
-
+            <v-list-tile v-for="item in products" :key="item.id" avatar @click.stop="dialog_on(item.id)">
               <v-list-tile-content>
-                <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                <v-list-tile-title v-text="item.name"></v-list-tile-title>
               </v-list-tile-content>
-
-              <v-chip v-model="chip4" close color="orange" label outline>Complete</v-chip>
             </v-list-tile>
           </v-list>
         </v-card-text>
       </v-card>
     </v-flex>
-    <v-flex xs12 sm5 md5 offset-xs0 offset-lg2>
-      <v-card dark color="green">
-        <v-card-text>
-          <v-list>
-            <v-list-tile v-for="item in items" :key="item.title" avatar @click>
-              <v-list-tile-action>
-                <v-icon v-if="item.icon" color="pink">star</v-icon>
-              </v-list-tile-action>
-
-              <v-list-tile-content>
-                <v-list-tile-title v-text="item.title"></v-list-tile-title>
-              </v-list-tile-content>
-
-              <v-list-tile-avatar>
-                <img :src="item.avatar">
-              </v-list-tile-avatar>
-            </v-list-tile>
-          </v-list>
-        </v-card-text>
-      </v-card>
-    </v-flex>
-    -->
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-flex xs12 sm5 md5>
-          <v-card dark color="primary">
-            <v-btn fab dark small color="indigo">
-              <v-icon dark>get</v-icon>
-            </v-btn>
-            <div class="text-xs-center">
-              <v-btn fab dark small color="green">
-                <v-icon dark>remove</v-icon>
-              </v-btn>
-              <v-btn fab dark small color="indigo" v-on="on">
-                <v-icon dark>add</v-icon>
-              </v-btn>
-            </div>
-            <v-card-text>
-              <v-list>
-                <v-list-tile v-for="item in products" :key="item.id" avatar >
-                  <v-list-tile-content>
-                    <v-list-tile-title v-text="item.name"></v-list-tile-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-              </v-list>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </template>
+    <v-dialog v-model="dialog.on"  max-width="600px">
       <v-card>
         <v-card-title>
-          <span class="headline">User Profile</span>
+          <span class="headline">상품</span>
         </v-card-title>
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
               <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal first name*" required></v-text-field>
+                <v-text-field label="상품이름*" required v-model="dialog.name"></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+                <v-text-field label="가격" v-model="dialog.price"></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
+
+               <v-flex xs12 sm6 md4>
+                <v-text-field label="옵션들" v-model="dialog.option_group"></v-text-field>
               </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Email*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6>
-                <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
-              </v-flex>
-              <v-flex xs12 sm6>
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-flex>
+
             </v-layout>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog.on = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog.on = false">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -146,7 +70,12 @@ export default {
   },
   data() {
     return {
-      dialog: false
+      dialog: {
+        on : false,
+        name: "",
+        price:0,
+        option_group:[]
+      }
     };
   },
   computed: {
@@ -158,7 +87,7 @@ export default {
     })
   },
   fetch({ store, params }) {
-    console.log("product.vue fetch start=====================")
+    console.log("product.vue fetch start=====================");
     store.dispatch("product/get", null).then(res => {
       console.log("store.dispatch(product/get, null)===>", res);
       if (res.status == 200) {
@@ -169,6 +98,17 @@ export default {
       }
     });
   },
-  mounted: function() {}
+  methods:  {
+    dialog_on(id){
+      console.log(id);
+      var found = this.products.find(function(element) {
+        return element.id == id;
+      });
+      this.dialog.name = found.name;
+      this.dialog.price = found.price;
+      this.dialog.option_group = found.option_group;
+      this.dialog.on = true;
+    }
+  }
 };
 </script>
