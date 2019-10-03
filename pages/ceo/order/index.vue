a035b6fd234963d2cec99a7c59a224d9a48196fa
chelgyu <rb7480@naver.com>
2019-10-04 03:46:20 +0900

    주문목록1

--- pages/ceo/order/index.vue
+++ pages/ceo/order/index.vue
@@ -3,46 +3,59 @@
   <template v-slot:header>
     <v-toolbar dark flat>
       <v-toolbar-title>주문 목록</v-toolbar-title>
+
     </v-toolbar>
   </template>
   <template v-slot:default="props">
-    <v-row>
-      <v-col v-for="item in props.items" :key="item.name" cols="3">
-        <v-card class="mx-auto" max-width="344">
+    <v-row v-for="item,i in props.items" :key="i">
+      <v-col>
+        <v-card class="mx-auto">
           <v-card-title>
-            <template v-if="item.state == 1">
-              <v-btn color="info" dark large @click.stop="fnitem(item, 1)">
-                승인
-              </v-btn>
-              <v-btn color="error" dark large @click.stop="fnitem(item, 0)">
-                거부
-              </v-btn>
-            </template>
-            <template v-else-if="(item.state = 2)">
-              <v-btn color="success" dark large @click.stop="fnitem(item, 2)">
-                제조완료
-              </v-btn>
-            </template>
+            <v-toolbar dense extended>
+              <v-toolbar-title>주문번호:{{item.id}}</v-toolbar-title>
+              <template #extension>
+                <v-spacer></v-spacer>
+                <v-toolbar-items>
+                  <template v-if="item.state == 1">
+
+                    <v-btn rounded color="info" @click.stop="fnitem(item, 1)">
+                      승인
+                    </v-btn>
+                    <v-btn rounded color="error" @click.stop="fnitem(item, 0)">
+                      거부
+                    </v-btn>
+                  </template>
+                  <template v-else-if="(item.state = 2)">
+                    <v-btn rounded color="success" @click.stop="fnitem(item, 2)">
+                      제조완료
+                    </v-btn>
+                  </template>
+                </v-toolbar-items>
+              </template>
+
+            </v-toolbar>
+
           </v-card-title>
           <v-card-text>
-            <v-card v-for="(p_group, j) in item.products" :key="'_' + j" cols="1" raised>
-              <v-card-title>
-                {{ p_group[0].name }} X {{ p_group.length }}
-              </v-card-title>
+            <v-card v-for="(p_group, j) in item.products" :key="i+j" cols="1" raised>
               <v-card-text>
-                <v-list>
-                  <v-list-tile-title v-for="p in p_group" :key="p.name">
-                    <v-chip-group column>
-                      <v-chip v-for="optg in p.option_group_list" :key="optg.name" :color="
-                            optg.select_opt_id == optg.default
-                              ? ''
-                              : 'deep-purple accent-4'
-                          ">
-                        {{ optg.select_opt_name }}
-                      </v-chip>
-                    </v-chip-group>
-                    <v-divider />
-                  </v-list-tile-title>
+                <v-list shaped>
+                  <v-subheader> {{ p_group[0].name }} X {{ p_group.length }}</v-subheader>
+                  <v-list-item-group>
+                    <v-list-item v-for="p,k in p_group" :key="i+j+k">
+                      <v-list-item-content>
+                        <v-chip-group column>
+                          <v-chip v-for="optg,n in p.option_group_list" :key="i+j+k+n" :color="
+                                optg.select_opt_id == optg.default
+                                  ? ''
+                                  : 'deep-purple accent-4'
+                              ">
+                            {{ optg.select_opt_name }}
+                          </v-chip>
+                        </v-chip-group>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list-item-group>
                 </v-list>
               </v-card-text>
             </v-card>
