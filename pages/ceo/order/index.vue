a4f4aa2907fad345fd17b837b3bdfe198f5a9090
chelgyu <rb7480@naver.com>
2019-10-04 01:16:31 +0900

    작업중

--- pages/ceo/order/index.vue
+++ pages/ceo/order/index.vue
@@ -1,38 +1,70 @@
 <template>
-  <v-data-iterator :items="order.now" hide-default-footer>
+  <v-data-iterator :items="order.now"
+hide-default-footer>
     <template v-slot:header>
-      <v-toolbar dark flat>
+      <v-toolbar dark
+flat>
         <v-toolbar-title>주문 목록</v-toolbar-title>
       </v-toolbar>
     </template>
     <template v-slot:default="props">
       <v-row>
-        <v-col v-for="item in props.items" :key="item.name" cols="3">
-          <v-card class="mx-auto" max-width="344">
+        <v-col v-for="item in props.items"
+:key="item.name" cols="3">
+          <v-card class="mx-auto"
+max-width="344">
             <v-card-title>
-              <template v-if="item.state==1">
-                <v-btn color="info" @click.stop="fnitem(item,1)" dark large>승인</v-btn>
-                <v-btn color="error" @click.stop="fnitem(item,0)" dark large>거부</v-btn>
+              <template v-if="item.state == 1">
+                <v-btn color="info"
+dark large @click.stop="fnitem(item, 1)">
+                  승인
+                </v-btn>
+                <v-btn color="error"
+dark large @click.stop="fnitem(item, 0)">
+                  거부
+                </v-btn>
               </template>
-              <template v-else-if="item.state=2">
-                <v-btn color="success" @click.stop="fnitem(item,2)" dark large>제조완료</v-btn>
+              <template v-else-if="(item.state = 2)">
+                <v-btn color="success"
+dark large @click.stop="fnitem(item, 2)">
+                  제조완료
+                </v-btn>
               </template>
             </v-card-title>
             <v-card-text>
-              <v-card v-for="(item , j) in item.products" :key="'_'+j" cols="1" raised>
-                <v-card-title>{{item.name}}</v-card-title>
-                <v-card-actions>
-                  <v-chip-group column>
-                    <v-chip
-                      v-for="item in item.option_group_list"
-                      :key="item.name"
-                      :color="item.select_opt_id == item.default ? '' : 'deep-purple accent-4'"
-                    >{{item.select_opt_name}}</v-chip>
-                  </v-chip-group>
-                </v-card-actions>
+              <v-card
+                v-for="(p_group, j) in item.products"
+                :key="'_' + j"
+                cols="1"
+                raised
+              >
+                <v-card-title>
+                  {{ p_group[0].name }} X {{ p_group.length }}
+                </v-card-title>
+                <v-card-text>
+                  <v-list>
+                    <v-list-tile-title v-for="p in p_group"
+:key="p.name">
+                      <v-chip-group column>
+                        <v-chip
+                          v-for="optg in p.option_group_list"
+                          :key="optg.name"
+                          :color="
+                            optg.select_opt_id == optg.default
+                              ? ''
+                              : 'deep-purple accent-4'
+                          "
+                        >
+                          {{ optg.select_opt_name }}
+                        </v-chip>
+                      </v-chip-group>
+                      <v-divider />
+                    </v-list-tile-title>
+                  </v-list>
+                </v-card-text>
               </v-card>
             </v-card-text>
-            <v-card-actions></v-card-actions>
+            <v-card-actions />
           </v-card>
         </v-col>
       </v-row>
@@ -52,14 +84,14 @@
 }
 </style>
 <script>
-import { mapState } from "vuex";
+import { mapState } from "vuex"
 
 export default {
   data: scope => ({ show: false }),
   fetch({ store, params }) {
-    console.log("=====================shop.vue fetch===================== ");
+    console.log("=====================shop.vue fetch===================== ")
     return store.dispatch("shop/chk_shop").then(res => {
-      console.log(res);
+      console.log(res)
     });
   },
   computed: {
@@ -76,10 +108,10 @@ export default {
         state: s,
         txt: this.shop,
         req_session_id: this.shop
-      };
-      console.log(" fnitem ", s, params);
-      this.$store.dispatch("order/state", params, { root: true });
+      }
+      console.log(" fnitem ", s, params)
+      this.$store.dispatch("order/state", params, { root: true })
     }
   }
-};
+}
 </script>
--- store/order.js
+++ store/order.js
@@ -1,101 +1,107 @@
 export const state = () => ({
-    cnt: 0,
-    icon: {
-        t: "notifications",
-        f: "notifications_none",
-        state: "notifications_none"
-    },
-    list: [],
-    now: []
-});
-
+  cnt: 0,
+  icon: {
+    t: "notifications",
+    f: "notifications_none",
+    state: "notifications_none"
+  },
+  list: [],
+  now: []
+})
+function groupBy(objectArray, property) {
+  return objectArray.reduce(function (acc, obj) {
+    var key = obj[property]
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(obj)
+    return acc
+  }, {})
+}
 export const actions = {
-    set_cnt({ commit, rootState }, params) {
-        commit("SET_CNT", params);
-    },
-    set_order({ commit, rootState }, params) {
-        console.log("set_order=", params);
-        commit("SET_ORDER", params);
-        commit("SET_CNT");
-    },
-    set_now({ commit, rootState }, params) {
-        console.log("set_now=", params);
-        commit("SET_NOW", params);
-    },
-    get_list({ commit, rootState, dispatch }, params) {
-        console.log("set_state=", params);
-        this.$axios
-            .get(
-                "/ceo/" +
-                rootState.user.auth.user.id +
-                "/shops/" +
-                rootState.shop.shop.id +
-                "/order"
-            )
-            .then(res => {
-                if (res.status == 200) {
-                    console.log("get_list  ", res.data.data.item)
-                    let r = res.data.data.item;
-                    dispatch("order/set_order", r, { root: true });
-
-                }
-            });
-    },
-    state({ commit, rootState, dispatch }, params) {
-        console.log("set_state=", params);
-        this.$axios
-            .put(
-                "/ceo/" +
-                rootState.user.auth.user.id +
-                "/shops/" +
-                rootState.shop.shop.id +
-                "/order_detail",
-                params
-            )
-            .then(res => {
-                if (res.status == 200) {
-                    dispatch("ws/send", params, { root: true });
-                    dispatch("order/now", params, { root: true });
-
-                } else {
-                }
-            });
-    },
-    now({ commit, rootState, dispatch }, params) {
-        this.$axios
-            .get(
-                "/ceo/" +
-                rootState.user.auth.user.id +
-                "/shops/" +
-                rootState.shop.shop.id +
-                "/order/now"
-            )
-            .then(res => {
-                if (res.status == 200) {
-                    console.log("now  ", res.data.data.item)
-                    let r = res.data.data.item;
-                    dispatch("order/set_now", r, { root: true });
-
-                }
-            });
-    },
-
-
-};
+  set_cnt({ commit, rootState }, params) {
+    commit("SET_CNT", params)
+  },
+  set_order({ commit, rootState }, params) {
+    console.log("set_order=", params)
+    commit("SET_ORDER", params)
+    commit("SET_CNT")
+  },
+  set_now({ commit, rootState }, params) {
+    console.log("set_now=", params)
+    commit("SET_NOW", params)
+  },
+  get_list({ commit, rootState, dispatch }, params) {
+    console.log("set_state=", params)
+    this.$axios
+      .get(
+        "/ceo/" +
+        rootState.user.auth.user.id +
+        "/shops/" +
+        rootState.shop.shop.id +
+        "/order"
+      )
+      .then(res => {
+        if (res.status == 200) {
+          console.log("get_list  ", res.data.data.item)
+          let r = res.data.data.item
+          dispatch("order/set_order", r, { root: true })
+        }
+      })
+  },
+  state({ commit, rootState, dispatch }, params) {
+    console.log("set_state=", params)
+    this.$axios
+      .put(
+        "/ceo/" +
+        rootState.user.auth.user.id +
+        "/shops/" +
+        rootState.shop.shop.id +
+        "/order_detail",
+        params
+      )
+      .then(res => {
+        if (res.status == 200) {
+          dispatch("ws/send", params, { root: true })
+          dispatch("order/now", params, { root: true })
+        } else {
+        }
+      })
+  },
+  now({ commit, rootState, dispatch }, params) {
+    this.$axios
+      .get(
+        "/ceo/" +
+        rootState.user.auth.user.id +
+        "/shops/" +
+        rootState.shop.shop.id +
+        "/order/now"
+      )
+      .then(res => {
+        if (res.status == 200) {
+          console.log("now  ", res.data.data.item)
+          let r = res.data.data.item
+          dispatch("order/set_now", r, { root: true })
+        }
+      })
+  }
+}
 
 export const mutations = {
-    SET_CNT(state) {
-        state.cnt++;
-    },
-    SET_ORDER(state, params) {
-        //state.list.push(params);
-        state.list = params;
-    },
-    SET_NOW(state, params) {
-        //state.list.push(params);
-        state.now = params;
-    },
-
-
-
-};
+  SET_CNT(state) {
+    state.cnt++
+  },
+  SET_ORDER(state, params) {
+    //state.list.push(params);
+    state.list = params
+  },
+  SET_NOW(state, params) {
+    //state.list.push(params);
+    console.log(params)
+    params.forEach(function (element) {
+      element.products = groupBy(element.products, "id")
+    });
+    console.log("params.products:", typeof params.products, params.products)
+    state.now = params
+  }
+}
