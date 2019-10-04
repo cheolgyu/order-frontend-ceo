1216dfe2531ed85b94cbb1a2035c5400afc3a5eb
cheolgyu <rb7480@naver.com>
2019-10-04 15:09:30 +0900

    주문내역

--- pages/ceo/order/index.vue
+++ pages/ceo/order/index.vue
@@ -4,12 +4,20 @@
       <thead>
         <tr>
           <th>주문번호</th>
-          <th></th>
+          <th>응답</th>
+          <th>가격</th>
+          <th>상품들</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(item,i) in order.list" v-bind:key="i">
           <td v-text="item.id"></td>
+          <td
+            v-text="item.state"
+          >COMMENT ON COLUMN "order"."state" IS '-2: 미응답거절, -1: 거절, 1: 대기, 2: 수락, 3: 수령 ';</td>
+          <td v-text="item.price"></td>
+
+          <td v-text="item.products"></td>
         </tr>
       </tbody>
     </table>
