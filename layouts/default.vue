7e2281df2ce6f0fe653f3c81dd8d3a613eaa7405
cheolgyu <rb7480@naver.com>
2019-10-19 11:05:59 +0900

    주문 정리

--- layouts/default.vue
+++ layouts/default.vue
@@ -67,26 +67,6 @@ export default {
         },
         {
           icon: "bubble_chart",
-          title: "주문1",
-          to: "/ceo/order/view1"
-        },
-        {
-          icon: "bubble_chart",
-          title: "주문2",
-          to: "/ceo/order/view2"
-        },
-        {
-          icon: "bubble_chart",
-          title: "주문3",
-          to: "/ceo/order/view3"
-        },
-        {
-          icon: "bubble_chart",
-          title: "전체",
-          to: "/ceo/order/list"
-        },
-        {
-          icon: "bubble_chart",
           title: "상점",
           to: "/ceo/shop"
         },
