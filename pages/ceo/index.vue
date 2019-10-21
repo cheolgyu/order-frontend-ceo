cf161a76159c799299ceee1ec6cc4d923388f450
cheolgyu <rb7480@naver.com>
2019-10-21 09:17:43 +0900

    ws url

--- pages/ceo/index.vue
+++ pages/ceo/index.vue
@@ -3,7 +3,6 @@
     <h1>Super secret page {{ auth }}</h1>
     <p>If you try to access this URL not connected, you will see the error page telling your that you are not connected.</p>
     <NuxtLink to="/">Back to the home page</NuxtLink>
-    <v-btn @click="conn" text="conn" />
   </div>
 </template>
 
@@ -21,17 +20,7 @@ export default {
       auth: state => state.user.auth
     })
   },
-  methods: {
-    conn() {
-      // The `fetch` method is used to fill the store before rendering the page
-      this.ws_conn = new WebSocket(this.wsUri);
-      this.ws_conn.onopen = function() {};
-      this.ws_conn.onmessage = function(e) {};
-      this.conn.onclose = function() {
-        this.ws_conn = null;
-        // update_ui()
-      };
-    }
+   
   }
 };
 /**
